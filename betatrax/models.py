ad990ad7b5233977608f7809cc8817c5f6e434e2
Jchan <20050910j@gmail.com>
2026-03-23 21:16:41 +0800

    Added models

--- betatrax/models.py
+++ betatrax/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class ProductOwner(models.Model):
+    name = models.CharField(max_length=200)
+
+class Developer(models.Model):
+    name = models.CharField(max_length=200)
+    assigned_id = models.CharField(max_length=200)
+
+class BetaTester(models.Model):
+    name = models.CharField(max_length=200)
+
+class DefectReport(models.Model):
+    name = models.CharField(max_length=200)
+    id =  models.CharField(max_length=200)
+    time = models.DateTimeField()
+    status = models.CharField(max_length=10)
--- demo/settings.py
+++ demo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'betatrax.apps.BetatraxConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
