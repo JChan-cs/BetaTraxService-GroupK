735717c359e78b89f396c0491d5852bbb60a040d
KWHK0626 <kwhk0626@connect.hku.hk>
2026-03-23 18:32:16 +0800

    Implement assigned defects list view
    Added a view to list defects assigned to the current user.

--- betatrax/views.py
+++ betatrax/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import DefectReport
 
-# Create your views here.
+@login_required
+def assigned_defects_list(request):
+    """Show all defects assigned to the current developer."""
+    defects = DefectReport.objects.filter(assigned_to=request.user, status='ASSIGNED')
+    return render(request, 'betatrax/assigned_defects.html', {'defects': defects})
