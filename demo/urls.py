c300a0f0f3e20429ac8783f9cd3eb2ae8ce838a8
KWHK0626 <kwhk0626@connect.hku.hk>
2026-03-23 18:33:10 +0800

    Add URL pattern for assigned defects list view

--- demo/urls.py
+++ demo/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+
+urlpatterns = [
+    path('assigned/', views.assigned_defects_list, name='assigned_defects'),
 ]
