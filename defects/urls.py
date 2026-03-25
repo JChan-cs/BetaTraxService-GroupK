1b953d69c4326f49bf6f4e8e0ca14b17fd927d1c
mct61674 <mct61674@connect.hku.hk>
2026-03-25 15:08:43 +0800

    Create urls.py

---
+++ defects/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DefectReportViewSet
+
+router = DefaultRouter()
+router.register(r"reports", DefectReportViewSet)
+urlpatterns = [path('',include(router.urls)),]
