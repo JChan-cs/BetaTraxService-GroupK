0c95e268b8e1d435d467e85d57cbc5ae3aab4372
SuperNicky12 <nickyl12@connect.hku.hk>
2026-03-25 11:39:56 +0800

    Fix missing import in demo/urls.py and commented out unimplemented views

--- betatrax/urls.py
+++ betatrax/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('open/', views.open_defects_list, name='open_defects'),
-    path('take/<int:defect_id>/', views.take_defect, name='take_defect'),
+    # path('open/', views.open_defects_list, name='open_defects'),
+    # path('take/<int:defect_id>/', views.take_defect, name='take_defect'),
     path('assigned/', views.assigned_defects_list, name='assigned_defects'),
     path('new/', views.new_defects_list, name='new_defects'),
 ]
