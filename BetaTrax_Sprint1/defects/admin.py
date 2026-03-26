f1f2e1c71a84c80da9390c79cb5da621edaf4c6a
mct61674 <mct61674@connect.hku.hk>
2026-03-26 14:46:35 +0800

    Update admin.py

--- BetaTrax_Sprint1/defects/admin.py
+++ BetaTrax_Sprint1/defects/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import DefectReport
 
-admin.site.register(Defect Report)
+admin.site.register(DefectReport)
 
 class DefectReportAdmin(admin.ModelAdmin):
-list_display = ("ProductID", "ReportTitle", "Status", "CreatedTime", "TesterID")
-list_display_links = ("ProductID", "Report Title")
-list_filter = ("Status", "Severity", "Priority", "CreatedTime")
-search_fields = ("ReportTitle", "Description", "TesterID", "ProductID")
-readonly_fields = ("CreatedTime", "UpdatedTime")
-fieldsets = (("Product Info", {"fields": ("ProductID", "Version", "ReportTitle", "Description", "Steps", "Status", "Severity", "Priority")}),("Tester Info", {"fields": ("TesterID", "Email")}),("Time", {"fields": ("CreatedTime", "UpdatedTime")}),)
+  list_display = ("ProductID", "ReportTitle", "Status", "CreatedTime", "TesterID")
+  list_display_links = ("ProductID", "Report Title")
+  list_filter = ("Status", "Severity", "Priority", "CreatedTime")
+  search_fields = ("ReportTitle", "Description", "TesterID", "ProductID")
+  readonly_fields = ("CreatedTime", "UpdatedTime")
+  fieldsets = (("Product Info", {"fields": ("ProductID", "Version", "ReportTitle", "Description", "Steps", "Status", "Severity", "Priority")}),("Tester Info", {"fields": ("TesterID", "Email")}),("Time", {"fields": ("CreatedTime", "UpdatedTime")}),)
