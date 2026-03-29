64efa4faeed6d2ee85bc3535b780ec27fa77e12a
Jchan <20050910j@gmail.com>
2026-03-29 13:13:26 +0800

    Revert "Partial fix to defects"
    This reverts commit 3e207b8cc28da200ff26e2ea9dc53f2888764084.

--- BetaTrax_Sprint1/defects/views.py
+++ BetaTrax_Sprint1/defects/views.py
@@ -37,7 +37,6 @@ class DefectReportViewSet(viewsets.ModelViewSet):
         methods=["patch"],
         url_path="status",
         permission_classes=[IsAuthenticated],
-        renderer_classes=[TemplateHTMLRenderer, JSONRenderer]
     )
     def change_status(self, request, pk=None):
         defect = self.get_object()
@@ -160,17 +159,13 @@ class DefectReportViewSet(viewsets.ModelViewSet):
             comments = Comment.objects.all().order_by('-created_at')[:50]
             context = {'defect': defect, 'comments': comments, 'errors': serializer.errors, 'severity_choices': DefectReport.SeverityC, 'priority_choices': DefectReport.PriorityC}
             return render(request, 'defect_evaluation.html', context)
-        
-    @action(detail=True, methods=["get"], url_path="evaluate-success", renderer_classes=[TemplateHTMLRenderer])
-    def evaluate_success(self, request, pk=None):
-        defect = self.get_object()
-        return render(request, 'evaluation_success.html', context)
+
         # GET logic
         comments = Comment.objects.all().order_by('-created_at')[:50]
         context = {'defect': defect, 'comments': comments, 'severity_choices': DefectReport.SeverityC, 'priority_choices': DefectReport.PriorityC}
-        return render(request, 'defect_evaluation.html', {'defect': defect})
+        return render(request, 'defect_evaluation.html', context)
 
-    @action(detail=False, methods=['get'], url_path='open', renderer_classes=[TemplateHTMLRenderer, JSONRenderer])
+    @action(detail=False, methods=['get'], url_path='open')
     def open_defects(self, request):
         defects = DefectReport.objects.filter(Status='Open')
         if request.accepted_renderer.format == 'json':
@@ -178,7 +173,7 @@ class DefectReportViewSet(viewsets.ModelViewSet):
             return Response(serializer.data) # Returns clean JSON
         return render(request, 'open_defects.html', {'defects': defects})
 
-    @action(detail=True, methods=['post'], url_path='take', permission_classes=[IsAuthenticated], renderer_classes=[TemplateHTMLRenderer, JSONRenderer])
+    @action(detail=True, methods=['post'], url_path='take', permission_classes=[IsAuthenticated])
     def take(self, request, pk=None):
         defect = self.get_object()
         # 1. CHECK FOR ERRORS FIRST
