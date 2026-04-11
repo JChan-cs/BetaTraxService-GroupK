3d1d26ef60a52aeb0a82a3aea8c1253dbcc3db1b
Jchan <20050910j@gmail.com>
2026-04-11 21:18:32 +0800

    Fixed urls

--- BetaTrax/defects/views.py
+++ BetaTrax/defects/views.py
@@ -155,23 +155,23 @@ class DefectReportViewSet(
             if user.groups.filter(name='Tester').exists():
                 role = "Tester"
                 links = [
-                    {"name": "Submit Defect Report", "url": "/api/reports/", "method": "POST"},
-                    {"name": "My Submissions", "url": f'/api/reports/?tester_id={user.id}', "method": "GET"},]
+                    {"name": "Submit Defect Report", "url": "/defects/reports/", "method": "POST"},
+                    {"name": "My Submissions", "url": f'/defects/reports/?tester_id={user.id}', "method": "GET"},]
             elif user.groups.filter(name='ProductOwner').exists():
                 role = "Product Owner"
-                links = [{'name': 'New Reports (Pending Evaluation)', 'url': '/api/reports/?Status=New', "method": "GET"},
-                    {"name": "Open Defects (Ready to Assign)", "url": '/api/reports/?Status=Open', "method": "GET"},
-                    {"name": "All Reports", 'url': '/api/reports/', "method": "GET"},
+                links = [{'name': 'New Reports (Pending Evaluation)', 'url': '/defects/reports/?Status=New', "method": "GET"},
+                    {"name": "Open Defects (Ready to Assign)", "url": '/defects/reports/?Status=Open', "method": "GET"},
+                    {"name": "All Reports", 'url': '/defects/reports/', "method": "GET"},
                 ]
             elif user.groups.filter(name='Developer').exists():
                 role = 'Developer'
-                links = [{"name": "My Assigned Tasks", "url": "/api/reports/?Status=Assigned", "method": "GET"},{"name": "Open Defects (Available)", 'url': '/api/reports/?Status=Open', "method": 'GET'},]
+                links = [{"name": "My Assigned Tasks", "url": "/defects/reports/?Status=Assigned", "method": "GET"},{"name": "Open Defects (Available)", 'url': '/defects/reports/?Status=Open', "method": 'GET'},]
             else:
                 role = 'Authenticated User (No Role)'
-                links = [{"name": "View All Reports", 'url': '/api/reports/', 'method': 'GET'},]
+                links = [{"name": "View All Reports", 'url': '/defects/reports/', 'method': 'GET'},]
         else:
             links = [{"name": "Login", "url": "/admin/login/", "method": "GET"},
-                {"name": "View All Reports (Read Only)", "url": "/api/reports/", "method": "GET"},]
+                {"name": "View All Reports (Read Only)", "url": "/defects/reports/", "method": "GET"},]
       
         # If the client accepts HTML, render a simple dashboard page.
         if request.accepted_renderer.format == 'html':
